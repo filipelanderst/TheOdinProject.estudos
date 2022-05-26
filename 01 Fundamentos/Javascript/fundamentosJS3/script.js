cd7595cda62b5d84d999092898e3aca56b7f60a6
Filipe Lander <flx-lander7@criptext.com>
2022-05-25 22:08:47 -0300

    fundamentos JS3

---
+++ 01 Fundamentos/Javascript/fundamentosJS3/script.js
@@ -0,0 +1,89 @@
+/* function volume(a, b, c) {
+  let mtsCubic = a * b * c;
+  console.log(mtsCubic);
+}
+
+volume(10, 10, 10); 
+
+function volume(a, b, c) {
+  return a * b * c;
+}
+
+console.log(volume(10, 10, 10)); 
+
+function volume(a, b, c) {
+  return a * b * c;
+}
+
+let mtsCubic = volume(20, 20, 20);
+
+console.log(mtsCubic); 
+
+function media(x, y) {
+  return (x + y) / 2;
+}
+
+console.log(media(10, 20)); 
+
+function notaAlunos(Laura, Jefferson, Camila, Raul) {
+  return (Laura + Jefferson + Camila + Raul) / notaAlunos.length;
+}
+
+console.log(notaAlunos(8, 9.5, 9, 8.5)); 
+
+let amor = 'loirinha';
+
+function msgAmor() {
+  let msg = 'Oi ' + amor + ' estava com saudade de você.';
+  return msg;
+}
+
+console.log(msgAmor()); 
+
+let age = prompt('Qual é a sua idade?: ');
+
+ function CarteiraMotorista() {
+  if (age > 18) {
+    console.log('Você pode tirar carteira.');
+  } else console.log('Você não pode tirar carteira.'); 
+}
+
+
+// Arrow function
+
+const soma = (a, b, c) => a + b + c;
+
+console.log(soma(10, 20, 30));
+
+const mult = (a, b, c) => a * b * c;
+
+console.log(mult(10, 20, 30));
+
+const invest = (c, j, t) => c + c * j * t;
+
+console.log(invest(1000, 0.1, 7)); 
+
+// Prática
+
+// 1. add7
+
+function add7(a) {
+  return a + 7;
+}
+
+console.log(add7(7)); 
+
+// 2. multiply
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(5, 6)); 
+
+// 3. capitalize
+
+let myString = 'lander';
+myString.charAt(0);
+
+*/
